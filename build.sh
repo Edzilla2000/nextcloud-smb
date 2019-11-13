ed63c1a2f4c5b794e18ce7afba31f8394599b344
root <root@lnx009.epa.grp>
2019-11-13 15:13:09 +0100

    update base image to 17.0.1

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker image build -f Dockerfile -t cardonaje/nextcloud:17.0.0-fpm  /srv/nextcloud-builder --pull
-docker push cardonaje/nextcloud:17.0.0-fpm
+docker image build -f Dockerfile -t cardonaje/nextcloud:17.0.1-fpm  /srv/nextcloud-builder --pull
+docker push cardonaje/nextcloud:17.0.1-fpm
 
